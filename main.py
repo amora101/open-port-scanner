706d73651fb05f707ac229d47d04cc1ad1af0f96
Meuz Kidane <95613515+meuzgebre@users.noreply.github.com>
2023-03-10 06:31:04 -0800

    fixing #3 issue

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from scanner.scanner import normal_scan
+import argparse
 import timeit
+import socket
 
-def hello():
-    print("Hello, world!")
+if __name__ == '__main__':    
 
-if __name__ == '__main__':
-    # normal_scan(target = '18.196.110.53')
-    # normal_scan(target = '18.196.110.53')
-    # timeit.timeit('normal_scan(target = "18.196.110.53")')    
-    t = timeit.timeit(hello, number=1)/10
-    print(f"{t:.6f}")
+    parser = argparse.ArgumentParser(description='Get IP address from domain name or vice versa')
+    parser.add_argument('input', type=str, help='Domain name or IP address')
+
+    args = parser.parse_args()
+
+    # Check if the input is an IP address or a domain name
+    try:
+        socket.inet_aton(args.input)
+        is_ip = True
+    except socket.error:
+        is_ip = False
+
+    # If the input is a domain name, get the IP address
+    if not is_ip:        
+        normal_scan(target = args.input)
+        # print(f'The IP address of {args.input} is {ip_address}')
+    else:        
+        normal_scan(target = args.input)
+        # domain_name = socket.gethostbyaddr(args.input)[0]
+        # print(f'The domain name of {args.input} is {domain_name}')
+    
+    t = timeit.timeit(normal_scan(target = args.input), number=1)/10
+    print(f"Operation completed in {t:.6f}")
